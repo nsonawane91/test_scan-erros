a9710be3ce54a2f975c74a4848c7ae8c01a8916a
nsonawane <95214663+nsonawane91@users.noreply.github.com>
2022-02-08 20:30:21 +0530

    Update acqa-repo1-aws-tf12-part2.tf

--- acqa-repo1-aws-tf12-part2.tf
+++ acqa-repo1-aws-tf12-part2.tf
@@ -8,7 +8,7 @@ resource "aws_acm_certificate" "acqa-test-acm1" {
   domain_name       = "acqatest.com"
   validation_method = "DNS"
 
-  tagsss = {
+  tags = {
     Name = format("%s-acm1", var.acqaPrefix)
     ACQAResource = "true"
     Owner = "AC-QA"
@@ -20,7 +20,7 @@ resource "aws_acm_certificate" "acqa-test-acm1" {
 }
 
 # Create SSM parameter resource
-resourceeee "aws_ssm_parameter" "acqa-test-ssmparam1" {
+resource "aws_ssm_parameter" "acqa-test-ssmparam1" {
   name  = "acqa-test-ssmparam1"
   type  = "String"
   value = "bar"
